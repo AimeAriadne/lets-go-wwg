a56fdd8d10c36c143b8b3e9817eccd46b16efc0a
AimeAriadne <aimeariadne@hotmail.com>
2021-07-10 17:51:03 -0300

    commit

---
+++ exercicioarray.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// escreva um programa que contenha um array de strings.
+// o valor de cada um dos elementos deve corresponder ao número do indice.
+// print na tela o tipo do seu array e os valores de seus emelentos.
+
+func main() {
+	var numeros [5]string
+	numeros[0] = "zero"
+	numeros[1] = "um"
+	numeros[2] = "dois"
+	numeros[3] = "três"
+	numeros[4] = "quatro"
+
+	fmt.Printf("o tipo é: %T\n", numeros)
+	fmt.Println(numeros)
+}
